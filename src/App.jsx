bb68891cb0756155629b455f9c5388f2050bc791
Mohammad Anzil <106994723+anzilparviz29@users.noreply.github.com>
2023-09-02 08:23:05 +0000

    Your commit message

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Anzil Parviz",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  email: "anzilparviz03@gmail.com",
+  gitHub: "anzilparviz29",
+  instagram: "anzil_parviz",
+  linkedIn: "mohammad-anzil-parveez-bab619244",
+ 
+  
 };
 
 const primaryColor = "#4E567E";
